c64d6edb0b95b8b9f8e3d79c7c9337a4af1d69b4
Peter Marton <email@martonpeter.com>
2017-12-03 15:42:38 +0100

    chore(docker): fix compose

--- src/models/dynamoDb.js
+++ src/models/dynamoDb.js
@@ -1,12 +1,13 @@
 const AWS = require('aws-sdk');
 
-const { IS_OFFLINE } = process.env;
-
+const { DYNAMODB_REGION, DYNAMODB_ENDPOINT } = process.env;
 const dynamoDbConfig = {};
 
-if (IS_OFFLINE) {
-  dynamoDbConfig.region = 'localhost';
-  dynamoDbConfig.endpoint = 'http://localhost:8000';
+if (DYNAMODB_REGION) {
+  dynamoDbConfig.region = DYNAMODB_REGION;
+}
+if (DYNAMODB_ENDPOINT) {
+  dynamoDbConfig.endpoint = DYNAMODB_ENDPOINT;
 }
 
 const documentClient = new AWS.DynamoDB.DocumentClient(dynamoDbConfig);
